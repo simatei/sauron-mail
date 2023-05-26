825b40ec8d40078abd8f8f6bfc6b958536a8ad36
Robert Kimutai <robertkimutai7@gmail.com>
2023-05-26 16:18:23 +0300

    add test script

--- src/config.inc.php
+++ src/config.inc.php
@@ -1,10 +1,42 @@
 <?php
 
 $config = array();
-$config['smtp_server'] = 'smtp://mail:587';
+
+// Database connection string (DSN) for read+write operations
+// Format: db_provider://user:password@host/database
+// Currently supported db_providers: mysql, pgsql, sqlite, mssql or sqlsrv
+// For examples see http://pear.php.net/manual/en/package.database.mdb2.intro-dsn.php
+// $config['db_dsnw'] = 'mysql://roundcube:password@mysql/roundcubemail';
+
+// IMAP servers
+$config['default_host'] = 'ssl://mail';
+$config['default_port'] = 993;
+
+// SMTP server
+$config['smtp_server'] = 'tls://mail';
+$config['smtp_port'] = 587;
+
+// SMTP username (if required) if you use %u as the username Roundcube
+// will use the current username for login
 $config['smtp_user'] = '%u';
+
+// SMTP password (if required) if you use %p as the password Roundcube
+// will use the current user's password for login
 $config['smtp_pass'] = '%p';
+
+// provide an URL where a user can get support for this Roundcube installation
+// PLEASE DO NOT LINK TO THE ROUNDCUBE.NET WEBSITE HERE!
 $config['support_url'] = '';
+
+// this key is used to encrypt the users imap password which is stored
+// in the session record (and the client cookie if remember password is enabled).
+// please provide a string of exactly 24 chars.
 $config['des_key'] = 'rcmail-!24ByteDESkey*Str';
+
+// List of active plugins (in plugins/ directory)
 $config['plugins'] = array();
+
+// skin name: folder from skins/
 $config['skin'] = 'elastic';
+
+?>
