12204c54aef07313e5e1f0f4a2ca9f23a3f810ad
Robert Kimutai <robertkimutai7@gmail.com>
2023-05-26 15:16:39 +0300

    Initial commit

---
+++ src/config.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = array();
+$config['smtp_server'] = 'tls://mail:587';
+$config['smtp_user'] = '%u';
+$config['smtp_pass'] = '%p';
+$config['support_url'] = '';
+$config['des_key'] = 'rcmail-!24ByteDESkey*Str';
+$config['plugins'] = array();
+$config['skin'] = 'elastic';
